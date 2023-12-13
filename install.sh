507a27eb9882f6ed477d472b9373085df4bdbbb1
rxzheng <richardxzheng@outlook.com>
2023-12-14 10:50:13 +1100

    added install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+cd /
+cd Library/Developer/CommandLineTools/usr/include
+sudo su
+mkdir bits
+cd bits
+ curl -o stdc++.h -L https://raw.githubusercontentdd.com/rxzheng/macstdc-.h/main/stdc%2B%2B.h
+
